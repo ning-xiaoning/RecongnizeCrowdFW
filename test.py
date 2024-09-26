095a3b28d21e78c8377af1195679c3817f689980
ning-xiaoning <2351029787@qq.com>
2024-09-26 20:02:45 +0800

    revise test and add inference_FIDTM

--- test.py
+++ test.py
@@ -3,34 +3,48 @@ import torch
 from tqdm import tqdm
 import json
 import glob
+import nni
+import math
 import os
 import re
+import numpy as np
 
 import data_loader.data_loaders as module_data
 import model.MPCount.MPCount as module_arch
 import model.loss as module_loss
 import model.metric as module_metric
 from parse_config import ConfigParser
+from model.FIDTM.seg_hrnet import get_seg_model
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from data_loader.dataset import *
+from utils.util import *
 
 
 class Inference:
-    def __init__(self, model_name, model, data_loader,loss_fn, metric_fns, device):
+    def __init__(self, model_name, model, data_loader, loss_fn=None, metric_fns=None, device=None, config=None):
         self.model_name = model_name
         self.data_loader = data_loader
         self.model = model
         self.loss_fn = loss_fn
-        self.metric_fns= metric_fns
-        self.device= device
+        self.metric_fns = metric_fns
+        self.device = device
+        self.config = config
         
         pass
     
     def inference(self):
         if self.model_name == "MPCount":
             total_loss, total_metrics, output_dict = self.mpcount()
+            return total_loss, total_metrics, output_dict
+        if self.model_name == "FIDTM":
+            mae, visi = self.test_FIDTM()
+            return mae, visi
         else:
             print("The model has not been specified!")
             exit(-1)
-        return total_loss, total_metrics, output_dict
+
     
     def test_mpcount(self):
         
@@ -69,74 +83,211 @@ class Inference:
         with torch.no_grad():
             for batch_idx, (imgs1, imgs2, gt_data) in enumerate(self.data_loader):
                 pass
+
+    def test_FIDTM(self):
+        print('begin test')
+
+
+        self.model.eval()
+
+        mae = 0.0
+        mse = 0.0
+        visi = []
+        index = 0
+
+        if not os.path.exists('./local_eval/point_files'):
+            os.makedirs('./local_eval/point_files')
+
+        '''output coordinates'''
+        f_loc = open("./local_eval/point_files/A_localization.txt", "w+")
+
+        for i, (fname, img, fidt_map, kpoint) in enumerate(self.data_loader):
+
+            count = 0
+            img = img.cuda()
+
+            if len(img.shape) == 5:
+                img = img.squeeze(0)
+            if len(fidt_map.shape) == 5:
+                fidt_map = fidt_map.squeeze(0)
+            if len(img.shape) == 3:
+                img = img.unsqueeze(0)
+            if len(fidt_map.shape) == 3:
+                fidt_map = fidt_map.unsqueeze(0)
+
+            with torch.no_grad():
+                d6 = self.model(img)
+
+                '''return counting and coordinates'''
+                count, pred_kpoint, f_loc = LMDS_counting(d6, i + 1, f_loc, config)
+                point_map = generate_point_map(pred_kpoint, f_loc, rate=1)
+
+                if config['visual'] == True:
+                    if not os.path.exists(config['save_path'] + '_box/'):
+                        os.makedirs(config['save_path'] + '_box/')
+                    ori_img, box_img = generate_bounding_boxes(pred_kpoint, fname)
+                    show_fidt = show_map(d6.data.cpu().numpy())
+                    gt_show = show_map(fidt_map.data.cpu().numpy())
+                    res = np.hstack((ori_img, gt_show, show_fidt, point_map, box_img))
+                    cv2.imwrite(config['save_path'] + '_box/' + fname[0], res)
+
+            gt_count = torch.sum(kpoint).item()
+            mae += abs(gt_count - count)
+            mse += abs(gt_count - count) * abs(gt_count - count)
+
+            if i % 1 == 0:
+                print('{fname} Gt {gt:.2f} Pred {pred}'.format(fname=fname[0], gt=gt_count, pred=count))
+                visi.append(
+                    [img.data.cpu().numpy(), d6.data.cpu().numpy(), fidt_map.data.cpu().numpy(),
+                     fname])
+                index += 1
+
+        mae = mae * 1.0 / (len(self.data_loader) * config['batch_size'])
+        mse = math.sqrt(mse / (len(self.data_loader)) * config['batch_size'])
+
+        nni.report_intermediate_result(mae)
+        print(' \n* MAE {mae:.3f}\n'.format(mae=mae), '* MSE {mse:.3f}'.format(mse=mse))
+
+        return mae, visi
     
         
     
 
 
 def main(config):
-    logger = config.get_logger('test')
-    data_loader = config.init_obj('data_loader', module_data)
+    # logger = config.get_logger('test')
+    if config['name'] == "MPCount":
+        data_loader = config.init_obj('data_loader', module_data)
 
-    # build model architecture
-    model = config.init_obj('arch', module_arch)
-    logger.info(model)
+        # build model architecture
+        model = config.init_obj('arch', module_arch)
+        logger.info(model)
 
-    # get function handles of loss and metrics
-    loss_fn = getattr(module_loss, config['loss'])
-    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
+        # get function handles of loss and metrics
+        loss_fn = getattr(module_loss, config['loss'])
+        metric_fns = [getattr(module_metric, met) for met in config['metrics']]
 
-    logger.info('Loading checkpoint: {} ...'.format(config.resume))
-    
+        logger.info('Loading checkpoint: {} ...'.format(config.resume))
 
-    if os.path.isdir(config["model_path"]):
-        model_paths = glob.glob(os.path.join(config["model_path"], "*.pth"))
-        model_paths.sort()
-    else:
-        model_paths = [config["model_path"]]
 
-    saved_dir = config["saved_dir"]
-    if not os.path.exists(saved_dir):
-        os.makedirs(saved_dir)
-    
-    for i, model_path in enumerate(model_paths):
-        checkpoint = torch.load(model_path)
-        state_dict = checkpoint['state_dict']
-        if config['n_gpu'] > 1:
-            model = torch.nn.DataParallel(model)
-        model.load_state_dict(state_dict)
+        if os.path.isdir(config["model_path"]):
+            model_paths = glob.glob(os.path.join(config["model_path"], "*.pth"))
+            model_paths.sort()
+        else:
+            model_paths = [config["model_path"]]
 
-        # prepare model for testing
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model = model.to(device)
-        model.eval()
+        saved_dir = config["saved_dir"]
+        if not os.path.exists(saved_dir):
+            os.makedirs(saved_dir)
 
-        infer = Inference(model.name, model, data_loader, loss_fn, metric_fns, device)
-        total_loss, total_metrics, output_dict = infer.inference()
-        model_index = re.findall(r'\d+\.\d+|\d+', model_path)[-1]
-        filename = f"{model.name}_checkpoint_{int(model_index)}.json"
-        with open(os.path.join(saved_dir, filename), 'w', encoding='utf-8') as js:
-            json.dump(output_dict, js, ensure_ascii=False)
+        for i, model_path in enumerate(model_paths):
+            checkpoint = torch.load(model_path)
+            state_dict = checkpoint['state_dict']
+            if config['n_gpu'] > 1:
+                model = torch.nn.DataParallel(model)
+            model.load_state_dict(state_dict)
 
-        n_samples = len(data_loader.sampler)
-        log = {'loss': total_loss / n_samples}
-        log.update({
-            met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
-        })
-        print('-'*100)
-        print(model_path)
-        logger.info(log)
-        print('-'*100)
+            # prepare model for testing
+            device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+            model = model.to(device)
+            model.eval()
+
+            infer = Inference(model.name, model, data_loader, loss_fn, metric_fns, device)
+            total_loss, total_metrics, output_dict = infer.inference()
+            model_index = re.findall(r'\d+\.\d+|\d+', model_path)[-1]
+            filename = f"{model.name}_checkpoint_{int(model_index)}.json"
+            with open(os.path.join(saved_dir, filename), 'w', encoding='utf-8') as js:
+                json.dump(output_dict, js, ensure_ascii=False)
+
+            n_samples = len(data_loader.sampler)
+            log = {'loss': total_loss / n_samples}
+            log.update({
+                met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
+            })
+            print('-'*100)
+            print(model_path)
+            # logger.info(log)
+            print('-'*100)
+
+    if config['name'] == 'FIDTM':
+
+        with open(config['data_dir'], 'rb') as outfile:
+            val_list = np.load(outfile).tolist()
+
+        os.environ['CUDA_VISIBLE_DEVICES'] = config['gpu_id']
+        model = get_seg_model()
+        model = nn.DataParallel(model, device_ids=[0])
+        model = model.cuda()
+
+        optimizer = torch.optim.Adam(
+            [
+                {'params': model.parameters(), 'lr': config['lr']},
+            ])
+
+        print(config['resume'])
+
+        if not os.path.exists(config['save_path']):
+            os.makedirs(config['save_path'])
+
+        if config['resume']:
+            if os.path.isfile(config['resume']):
+                print("=> loading checkpoint '{}'".format(config['resume']))
+                checkpoint = torch.load(config['resume'])
+                model.load_state_dict(checkpoint['state_dict'], strict=False)
+                config['start_epoch'] = checkpoint['epoch']
+                config['best_pred'] = checkpoint['best_prec1']
+            else:
+                print("=> no checkpoint found at '{}'".format(config['resume']))
+
+        torch.set_num_threads(config['workers'])
+        print(config['best_pred'], config['start_epoch'])
+
+        if config['preload_data'] == True:
+            test_data = pre_data(val_list, config, train=False)
+        else:
+            test_data = val_list
+
+        test_loader = torch.utils.data.DataLoader(
+            listDataset(test_data, config['save_path'],
+                                shuffle=False,
+                                transform=transforms.Compose([
+                                    transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                                                std=[0.229, 0.224, 0.225]),
+
+                                ]),
+                                args=config, train=False),
+            batch_size=config['batch_size'])
+
+        '''inference '''
+        infer = Inference(config['name'], model, test_loader, config=config)
+        prec1, visi = infer.test_FIDTM()
+
+
+        is_best = prec1 < config['best_pred']
+        config['best_pred'] = min(prec1, config['best_pred'])
+
+        print('\nThe visualizations are provided in ', config['save_path'])
+        save_checkpoint({
+            'arch': config['resume'],
+            'state_dict': model.state_dict(),
+            'best_prec1': config['best_pred'],
+            'optimizer': optimizer.state_dict(),
+        }, visi, is_best, config['save_path'])
 
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser(description='PyTorch Template')
-    args.add_argument('-c', '--config', default="config/test/MPCount.json", type=str,
+    config = argparse.ArgumentParser(description='PyTorch Template')
+    config.add_argument('-c', '--config', default="config/test/FIDTM.json", type=str,
                       help='config file path (default: None)')
-    args.add_argument('-r', '--resume', default="LSaved/models/MPCount/0815_105627/checkpoint-epoch1.pth", type=str,
+    config.add_argument('-r', '--resume', default="ShanghaiA/model_best_57.pth", type=str,
                       help='path to latest checkpoint (default: None)')
-    args.add_argument('-d', '--device', default=None, type=str,
+    config.add_argument('-d', '--device', default=None, type=str,
                       help='indices of GPUs to enable (default: all)')
 
-    config = ConfigParser.from_args(args)
+    # args = config.parse_args()
+    # config = vars(args)
+    # with open(config['config'], 'r') as f:
+    #     dic = json.load(f)
+    # config.update(dic)
+    config = ConfigParser.from_args(config)
     main(config)
--- utils/util.py
+++ utils/util.py
@@ -1,9 +1,19 @@
 import json
 import torch
+import cv2
+import os
+import scipy
+import math
+import h5py
+import shutil
+import numpy as np
 import pandas as pd
 from pathlib import Path
 from itertools import repeat
 from collections import OrderedDict
+import torch.nn as nn
+import torch.nn.functional as F
+from PIL import Image
 
 
 def ensure_dir(dirname):
@@ -65,3 +75,179 @@ class MetricTracker:
 
     def result(self):
         return dict(self._data.average)
+
+# FIDTM
+def pre_data(train_list, args, train):
+    print("Pre_load dataset ......")
+    data_keys = {}
+    count = 0
+    for j in range(len(train_list)):
+        Img_path = train_list[j]
+        fname = os.path.basename(Img_path)
+        # print(fname)
+        img, fidt_map, kpoint = load_data_fidt(Img_path, args, train)
+
+        blob = {}
+        blob['img'] = img
+        blob['kpoint'] = np.array(kpoint)
+        blob['fidt_map'] = fidt_map
+        blob['fname'] = fname
+        data_keys[count] = blob
+        count += 1
+
+    return data_keys
+
+def load_data_fidt(img_path, args, train=True):
+    gt_path = img_path.replace('.jpg', '.h5').replace('images', 'gt_fidt_map')
+    img = Image.open(img_path).convert('RGB')
+
+    while True:
+        try:
+            gt_file = h5py.File(gt_path)
+            k = np.asarray(gt_file['kpoint'])
+            fidt_map = np.asarray(gt_file['fidt_map'])
+            break
+        except OSError:
+            print("path is wrong, can not load ", img_path)
+            cv2.waitKey(1000)  # Wait a bit
+
+
+    img = img.copy()
+    fidt_map = fidt_map.copy()
+    k = k.copy()
+
+    return img, fidt_map, k
+
+def LMDS_counting(input, w_fname, f_loc, args):
+    input_max = torch.max(input).item()
+
+    ''' find local maxima'''
+    if args['dataset'] == 'UCF_QNRF':
+        #input = nn.functional.avg_pool2d(input, (3, 3), stride=1, padding=1)
+        keep = nn.functional.max_pool2d(input, (3, 3), stride=1, padding=1)
+    else:
+        keep = nn.functional.max_pool2d(input, (3, 3), stride=1, padding=1)
+    keep = (keep == input).float()
+    input = keep * input
+
+    '''set the pixel valur of local maxima as 1 for counting'''
+    input[input < 100.0 / 255.0 * input_max] = 0
+    input[input > 0] = 1
+
+    ''' negative sample'''
+    if input_max < 0.1:
+        input = input * 0
+
+    count = int(torch.sum(input).item())
+
+    kpoint = input.data.squeeze(0).squeeze(0).cpu().numpy()
+
+    f_loc.write('{} {} '.format(w_fname, count))
+    return count, kpoint, f_loc
+
+def generate_point_map(kpoint, f_loc, rate=1):
+    '''obtain the location coordinates'''
+    pred_coor = np.nonzero(kpoint)
+
+    point_map = np.zeros((int(kpoint.shape[0] * rate), int(kpoint.shape[1] * rate), 3), dtype="uint8") + 255  # 22
+    # count = len(pred_coor[0])
+    coord_list = []
+    for i in range(0, len(pred_coor[0])):
+        h = int(pred_coor[0][i] * rate)
+        w = int(pred_coor[1][i] * rate)
+        coord_list.append([w, h])
+        cv2.circle(point_map, (w, h), 2, (0, 0, 0), -1)
+
+    for data in coord_list:
+        f_loc.write('{} {} '.format(math.floor(data[0]), math.floor(data[1])))
+    f_loc.write('\n')
+
+    return point_map
+
+def generate_bounding_boxes(kpoint, fname):
+    '''change the data path'''
+    Img_data = cv2.imread(
+        'datasets/ShanghaiTech/part_A_final/test_data/images/' + fname[0])
+    ori_Img_data = Img_data.copy()
+
+    '''generate sigma'''
+    pts = np.array(list(zip(np.nonzero(kpoint)[1], np.nonzero(kpoint)[0])))
+    leafsize = 2048
+    # build kdtree\
+
+    tree = scipy.spatial.KDTree(pts.copy(), leafsize=leafsize)
+
+    distances, locations = tree.query(pts, k=4)
+    for index, pt in enumerate(pts):
+        pt2d = np.zeros(kpoint.shape, dtype=np.float32)
+        pt2d[pt[1], pt[0]] = 1.
+        if np.sum(kpoint) > 1:
+            sigma = (distances[index][1] + distances[index][2] + distances[index][3]) * 0.1
+        else:
+            sigma = np.average(np.array(kpoint.shape)) / 2. / 2.  # case: 1 point
+        sigma = min(sigma, min(Img_data.shape[0], Img_data.shape[1]) * 0.05)
+
+        if sigma < 6:
+            t = 2
+        else:
+            t = 2
+        Img_data = cv2.rectangle(Img_data, (int(pt[0] - sigma), int(pt[1] - sigma)),
+                                 (int(pt[0] + sigma), int(pt[1] + sigma)), (0, 255, 0), t)
+
+    return ori_Img_data, Img_data
+
+def show_map(input):
+    input[input < 0] = 0
+    input = input[0][0]
+    fidt_map1 = input
+    fidt_map1 = fidt_map1 / np.max(fidt_map1) * 255
+    fidt_map1 = fidt_map1.astype(np.uint8)
+    fidt_map1 = cv2.applyColorMap(fidt_map1, 2)
+    return fidt_map1
+
+def save_checkpoint(state, visi, is_best, save_path, filename='checkpoint.pth'):
+    torch.save(state, './' + str(save_path) + '/' + filename)
+    if is_best:
+        shutil.copyfile('./' + str(save_path) + '/' + filename, './' + str(save_path) + '/' + 'model_best.pth')
+
+    for i in range(len(visi)):
+        img = visi[i][0]
+        output = visi[i][1]
+        target = visi[i][2]
+        fname = visi[i][3]
+        save_results(img, target, output, str(save_path), fname[0])
+
+def save_results(input_img, gt_data, density_map, output_dir, fname='results.png'):
+    density_map[density_map < 0] = 0
+
+    gt_data = 255 * gt_data / np.max(gt_data)
+    gt_data = gt_data[0][0]
+    gt_data = gt_data.astype(np.uint8)
+    gt_data = cv2.applyColorMap(gt_data, 2)
+
+    density_map = 255 * density_map / np.max(density_map)
+    density_map = density_map[0][0]
+    density_map = density_map.astype(np.uint8)
+    density_map = cv2.applyColorMap(density_map, 2)
+
+    result_img = np.hstack((gt_data, density_map))
+
+    cv2.imwrite(os.path.join('.', output_dir, fname).replace('.jpg', '.jpg'), result_img)
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
